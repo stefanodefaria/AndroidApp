81e3253e559142f6e8e14db1495183938233340a
stefano <stefanodefaria@gmail.com>
2015-05-18 20:26:00 -0300

    Movi o App para esse novo git.

---
+++ app/src/main/java/com/unifei/stefano/lab_ead_app/activities/ActivityExpForm.java
@@ -0,0 +1,55 @@
+package com.unifei.stefano.lab_ead_app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.unifei.stefano.lab_ead_app.Controller;
+import com.unifei.stefano.lab_ead_app.R;
+
+
+public class ActivityExpForm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exp_form);
+
+
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setLogo(R.mipmap.ic_launcher);
+//        actionBar.setDisplayUseLogoEnabled(true);
+//        actionBar.setDisplayShowHomeEnabled(true);
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Controller.setmTelaExpForm(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_experimento_gravidade, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View v){
+//        Controller.iniciarOperacaoLogin(this);
+    }
+
+}
