81f8ea70a7a14e898d38f54009589da5acad3b4a
Felipe Natale <felipenatale@gmail.com>
2015-08-03 19:05:51 -0300

    CustomListView

--- app/src/main/java/com/unifei/stefano/lab_ead_app/activities/ActivityExpForm.java
+++ app/src/main/java/com/unifei/stefano/lab_ead_app/activities/ActivityExpForm.java
@@ -2,9 +2,15 @@ package com.unifei.stefano.lab_ead_app.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +26,9 @@ public class ActivityExpForm extends Activity {
     private ListView mlistHints;
     private ArrayList<String> mCamposList;
     private ArrayList<String> mHintsList;
+    private ArrayList<String> arrText;
+    private ArrayList<String> arrHint;
+    private String[] arrTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,50 +37,17 @@ public class ActivityExpForm extends Activity {
 
         Controller.setmTelaExpForm(this);
 
-
         TextView mExpNameViewForm = (TextView) findViewById(R.id.exp_nameForm);
         Bundle b = getIntent().getExtras();
         mExpNameViewForm.setText(b.getString("expName"));
+        arrText = b.getStringArrayList("expFormCampos");
+        arrHint = b.getStringArrayList("expFormHints");
+        //TODO Colocar os hints na edittext
+        arrTemp = new String[arrText.size()];
 
-
-      //  mlistCampos = (ListView) findViewById(R.id.campos_list);
-      //  mlistHints = (ListView) findViewById(R.id.hints_list);
-      //  ArrayList<String> arrText = b.getStringArrayList("expFormCampos");
-
-        //mCamposList = b.getStringArrayList("expFormCampos");
-        // mHintsList = b.getStringArrayList("expFormHints");
-/*
-        ArrayList<String> lista = new ArrayList<String>();
-        for(int i=0 ; i<mCamposList.size() ; i++){
-            lista.add(mCamposList.get(i));
-
-        }
-
-        ArrayAdapter<String> arrayAdapterList = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                lista);
-
-        mlistHints.setAdapter(arrayAdapterList);
-*/
-/*
-    //TODO Arrumar infos do formulario
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                b.getStringArrayList("expFormCampos"));
-
-        mlistCampos.setAdapter(arrayAdapter);
-
-        ArrayAdapter<String> arrayAdapterHints = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                b.getStringArrayList("expFormHints"));
-
-        mlistHints.setAdapter(arrayAdapterHints);
-
-*/
-
+        MyListAdapter myListAdapter = new MyListAdapter();
+        ListView listView = (ListView) findViewById(R.id.campos_list);
+        listView.setAdapter(myListAdapter);
 
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setLogo(R.mipmap.ic_launcher);
@@ -82,7 +58,86 @@ public class ActivityExpForm extends Activity {
 
     }
 
+    private class MyListAdapter extends BaseAdapter {
 
+        @Override
+        public int getCount() {
+
+            if(arrText != null && arrText.size() != 0){
+                return arrText.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+
+            return arrText.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            //ViewHolder holder = null;
+            final ViewHolder holder;
+            if (convertView == null) {
+
+                holder = new ViewHolder();
+                LayoutInflater inflater = ActivityExpForm.this.getLayoutInflater();
+                convertView = inflater.inflate(R.layout.itens, null);
+                holder.textView1 = (TextView) convertView.findViewById(R.id.textView1);
+                holder.editText1 = (EditText) convertView.findViewById(R.id.editText1);
+
+                convertView.setTag(holder);
+
+            } else {
+
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.ref = position;
+
+            holder.textView1.setText(arrText.get(position));
+            holder.editText1.setText(arrTemp[position]);
+            holder.editText1.addTextChangedListener(new TextWatcher() {
+
+                @Override
+                public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+
+
+                }
+
+                @Override
+                public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                              int arg3) {
+
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable arg0) {
+
+                    arrTemp[holder.ref] = arg0.toString();
+                }
+            });
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView textView1;
+            EditText editText1;
+            int ref;
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -95,16 +150,6 @@ public class ActivityExpForm extends Activity {
         this.mCamposList = expCampos;
 
     }
- /*
-    private void atualiza(){
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                mCamposList );
-
-        mlistView.setAdapter(arrayAdapter);
-    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
